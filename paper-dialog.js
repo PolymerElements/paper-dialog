6b130a026dc15bf142d6b06bbb1e2b139ef127d6
Elliott Marquez <emarquez@google.com>
2018-06-22 17:04:40 -0700

    auto-converted by polymer-modulizer

--- paper-dialog.js
+++ paper-dialog.js
@@ -1,4 +1,4 @@
-<!--
+/**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
@@ -6,13 +6,8 @@ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
 The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
-
-<link rel="import" href="../polymer/polymer.html">
-<link rel="import" href="../neon-animation/neon-animation-runner-behavior.html">
-<link rel="import" href="../paper-dialog-behavior/paper-dialog-behavior.html">
-<link rel="import" href="../paper-dialog-behavior/paper-dialog-shared-styles.html">
-<!--
+*/
+/**
 Material design: [Dialogs](https://www.google.com/design/spec/components/dialogs.html)
 
 `<paper-dialog>` is a dialog with Material Design styling and optional animations when it is
@@ -66,39 +61,44 @@ element.
 @element paper-dialog
 @hero hero.svg
 @demo demo/index.html
--->
+*/
+/*
+  FIXME(polymer-modulizer): the above comments were extracted
+  from HTML and may be out of place here. Review them and
+  then delete this comment!
+*/
+import '@polymer/polymer/polymer-legacy.js';
 
-<dom-module id="paper-dialog">
-  <template>
+import { NeonAnimationRunnerBehavior } from '@polymer/neon-animation/neon-animation-runner-behavior.js';
+import { PaperDialogBehavior } from '@polymer/paper-dialog-behavior/paper-dialog-behavior.js';
+import '@polymer/paper-dialog-behavior/paper-dialog-shared-styles.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+Polymer({
+  _template: html`
     <style include="paper-dialog-shared-styles"></style>
     <slot></slot>
-  </template>
-</dom-module>
+`,
 
-<script>
-  Polymer({
-    is: 'paper-dialog',
+  is: 'paper-dialog',
+  behaviors: [PaperDialogBehavior, NeonAnimationRunnerBehavior],
+  listeners: {'neon-animation-finish': '_onNeonAnimationFinish'},
 
-    behaviors: [Polymer.PaperDialogBehavior, Polymer.NeonAnimationRunnerBehavior],
+  _renderOpened: function() {
+    this.cancelAnimation();
+    this.playAnimation('entry');
+  },
 
-    listeners: {'neon-animation-finish': '_onNeonAnimationFinish'},
+  _renderClosed: function() {
+    this.cancelAnimation();
+    this.playAnimation('exit');
+  },
 
-    _renderOpened: function() {
-      this.cancelAnimation();
-      this.playAnimation('entry');
-    },
-
-    _renderClosed: function() {
-      this.cancelAnimation();
-      this.playAnimation('exit');
-    },
-
-    _onNeonAnimationFinish: function() {
-      if (this.opened) {
-        this._finishRenderOpened();
-      } else {
-        this._finishRenderClosed();
-      }
+  _onNeonAnimationFinish: function() {
+    if (this.opened) {
+      this._finishRenderOpened();
+    } else {
+      this._finishRenderClosed();
     }
-  });
-</script>
+  }
+});
